a3b93631a0a787b997b76cd07b5fd3bf4996c179
achyuth13 <achyuthgollapudi@gmail.com>
2024-09-18 19:47:37 +0530

    Add layout.server.ts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import '../app.css';
-    export const prerender = true;
 </script>
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
