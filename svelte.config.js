23ce0f014d3d3f5db6b773ab0a708d24252e53d4
achyuth13 <achyuthgollapudi@gmail.com>
2024-09-18 18:24:27 +0530

    Add gh pages configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,19 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	]
+  preprocess: preprocess({
+    postcss: true
+  }),
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: '200.html',
+	  strict: false
+    }),
+  },
 };
 
 export default config;
